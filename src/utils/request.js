46dc7f09b5c9b3ed24e1f3a5b4ffc90a678c835d
Mahezer Carvalho de Melo <mahezercarvalho@gmail.com>
2017-10-21 16:45:04 -0200

    Aperfeicoar request util

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,18 +2,26 @@
 var sql = require('../database/connection')
 
 function makeRequest(query) {
-  sql.connection()
-    .then(pool => {
-      return pool.request()
-        .query(query);
-    })
-  .then(result => {
-    console.log(result);
+  return new Promise((resolve, reject) => {
+    try {
+      sql.connection()
+        .then(pool => {
+          return pool.request()
+            .query(query);
+        })
+      .then(result => {
+        pool.close();
+        resolve(result);
+      })
+      .catch(err => {
+        console.log(err);
+       })
+    } catch (err) {
+      reject(err);
+    }
+
   })
-  .catch(err => {
-    console.log(err);
-  })
-  
+
 }
 
 module.exports = { 
