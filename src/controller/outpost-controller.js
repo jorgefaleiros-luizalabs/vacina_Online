85c90911617071c9734976a83da644b8199572e5
Jorge Souza Faleiros Filho <jfilho@jorgesff.notebook>
2017-10-22 06:19:13 -0200

    add numero e cep outpost

--- src/controller/outpost-controller.js
+++ src/controller/outpost-controller.js
@@ -5,7 +5,7 @@ var dateUtil = require('../utils/date');
 function getOutpost(){
     return new Promise((resolve, reject) => {
         try {
-            sqlRequest.makeRequest('SELECT u.Nome_Fantasia, u.Telefone, r.Endereco \
+            sqlRequest.makeRequest('SELECT u.Nome_Fantasia, u.Telefone, r.Endereco, u.Nr_Casa, u.CEP \
                 FROM hackatonpreta.dbo.unidades as u, hackatonpreta.dbo.AUX_rua as r \
                 where u.Cod_Tp_Unidades = 2 and r.Cd_Rua = u.Cd_Rua;')
             .then((response) => {
@@ -16,10 +16,14 @@ function getOutpost(){
                         "nome": null,
                         "telefone": null,
                         "endereco": null,
+                        "numero": null,
+                        "cep": null
                     }
                     item.nome = element.Nome_Fantasia;
                     item.telefone = element.Telefone;
                     item.endereco = element.Endereco;
+                    item.cep = element.CEP;
+                    item.numero = element.Nr_Casa;
                     payload.push(item);
                 }, this);
                 resolve(payload);
